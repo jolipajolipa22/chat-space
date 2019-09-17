e800e06c009b5174932548d06176be03c7712e43
jolipajolipa22 <jolipajolipa22@gmail.com>
2019-09-17 15:57:53 +0900

    group-sakusei

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,22 @@
 class UsersController < ApplicationController
 
+  def index
+  end
+
   def edit
   end
 
   def update
-    if current_user.update(user_params)
-      redirect_to root_path
-    else
-      render :edit
-    end
+    # if current_user.update(user_params)
+    #   redirect_to root_path
+    # else
+    #   render :edit
+    # end
   end
 
-  private
+  # private
 
-  def user_params
-    params.require(:user).permit(:name, :email)
-  end
+  # def user_params
+  #   params.require(:user).permit(:name, :email)
+  # end
 end
